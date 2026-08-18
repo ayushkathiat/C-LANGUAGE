18207489ffbc42a3653f18fb1c1821699b5f01d0
Ayush Kathait <ayushkathait009@gmail.com>
2026-08-18 13:18:16 +0530

    UNDATED

--- 09_CH9(2D-ARRAY)/01_Probelm.c
+++ 09_CH9(2D-ARRAY)/01_Probelm.c
@@ -10,6 +10,13 @@ int main(){
             printf("%d ", arr[i][j]);
          }printf("\n");
     }
+    printf("\n");
+    
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 4; j++){
+            printf("%d ", arr[i][j]);
+         }printf("\n");
+    }
 
     return 0;
 } 
