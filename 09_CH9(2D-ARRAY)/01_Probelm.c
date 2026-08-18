b71a9da24bf37e56e9c1811632b8b1f4daf3276b
Ayush Kathait <ayushkathait009@gmail.com>
2026-08-18 13:01:58 +0530

    NEW

---
+++ 09_CH9(2D-ARRAY)/01_Probelm.c
@@ -0,0 +1,15 @@
+// WAP TO STORE ROLL BUM AND MARKS OBTAINED BY 4 STUDENTS SIDE 
+// BY  SIDE IN A MATRIX
+
+#include <stdio.h>
+int main(){
+    int arr[4][2] = {{8, 90}, {12, 23}, {34, 56}, {34, 79}};
+    
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 2; j++){
+            printf("%d ", arr[i][j]);
+         }printf("\n");
+    }
+
+    return 0;
+} 
