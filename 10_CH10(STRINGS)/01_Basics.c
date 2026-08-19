b7a12687c01852ce5b805b8329ba0257f61a45cd
Ayush Kathait <ayushkathait009@gmail.com>
2026-08-19 18:24:17 +0530

    UBDATED

---
+++ 10_CH10(STRINGS)/01_Basics.c
@@ -0,0 +1,34 @@
+// #include <stdio.h>
+// int main(){ 
+//     char arr[] = {'a', 'y', 'u', 's', 'h'};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     for(int i = 0; i < n; i++){
+//         printf("%c", arr[i]);
+//     }
+//     printf("\n");
+
+//     return 0;
+// }
+
+// OR
+// #include <stdio.h>
+// int main(){ 
+//     char arr[] = "Ayush OP";
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     for(int i = 0; i < n; i++){
+//         printf("%c", arr[i]);
+//     }
+//     printf("\n");
+
+//     return 0;
+// }
+
+// OR
+#include <stdio.h>
+int main(){
+    char arr[] = "Ayush OP";
+    printf("%s", arr);
+
+    return 0;
+}
+  
---
+++ 10_CH10(STRINGS)/02_str_with_size.c
@@ -0,0 +1,9 @@
+// STRING WITH GIVEN SIZE
+
+#include <stdio.h>
+int main(){
+    char str[20] = "Ayush Kathiat";
+    printf("%s", str);
+
+    return 0;
+} 
