02e7905e287b35f78ae384c2cbf6dbe54a1a3a4a
Ayush Kathait <ayushkathait009@gmail.com>
2026-09-05 20:40:59 +0530

    UBDATED

--- 11_CH11(STRUCTURES)/01_basiceofstructure.c
+++ 11_CH11(STRUCTURES)/01_basiceofstructure.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 int main(){
-    struct pokimon{    // user defined data type 
+    struct pokemon{    // user defined data type 
         int hp;
         int speed;
         int attack;
         char tier;     // S,A,B,C,D
     }pikachu, charizard, mewtow;
-
+  
     // pikachu.attack = 1000;
     printf("Enter the pikachu attack : ");
     scanf("%d", &pikachu.attack);
     pikachu.hp = 100;
     pikachu.speed = 50;
     pikachu.tier = 's';
-
-    printf("%d", pikachu.attack);
+    printf("--- Pikachu ---\n");
+    printf(" Attack - %d\n HP - %d\n Speed - %d\n Tier - %c\n", pikachu.attack, pikachu.hp, pikachu.speed, pikachu.tier);
 
     charizard.attack = 780;
     charizard.hp = 500;
     charizard.speed = 20;
     charizard.tier = 'B';
+    printf("\n--- Charizard ---\n");
+    printf(" Attack - %d\n HP - %d\n Speed - %d\n Tier - %c\n", charizard.attack, charizard.hp, charizard.speed, charizard.tier);
 
     mewtow.attack = 800;
     mewtow.hp = 200;
     mewtow.speed = 20;
     mewtow.tier = 'S';
+    printf("\n--- Mewtow ---\n");
+    printf(" Attack - %d\n HP - %d\n Speed - %d\n Tier - %c\n",mewtow.attack, mewtow.hp,mewtow.speed,mewtow.tier);
 
     return 0;
 }
